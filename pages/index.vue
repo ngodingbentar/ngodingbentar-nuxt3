81e5541c002d2d5f4f2f17998c451c48b5b3365d
ngodingbentar <dewaqintoro@gmail.com>
2023-04-15 16:27:37 +0700

    added: @carbon/icons-vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div><button @click="cek">cek</button></div>
   <h1 class="text-3xl font-bold underline text-blue-400">
-    Hello world!
+    Hello world! <IconChevronDown />
   </h1>
 </template>
 
 <script setup>
-// const { data: count } = await useFetch('/api/count')
+import IconChevronDown from '@carbon/icons-vue/es/chevron--down/16'
+
 async function cek () {
   const axios = useNuxtApp().$axios
   // const data = await useFetch('/api/province')
